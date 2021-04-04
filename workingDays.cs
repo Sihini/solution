72abcbd474f471e3d664d2725374abf4bec3c0ce
Sihini <68741342+Sihini@users.noreply.github.com>
2021-04-04 18:59:27 +0530

    Working days form designed

---
+++ workingDays.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace solution
+{
+    public partial class workingDays : Form
+    {
+        public workingDays()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
